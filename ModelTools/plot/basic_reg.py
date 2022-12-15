8338c4f670a4f937bf05983d320c52360bd57e4d
ZhangShenhao <zhangshenhao@outlook.com>
2022-12-15 23:35:19 +0800

    update

--- ModelTools/plot/basic_reg.py
+++ ModelTools/plot/basic_reg.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import statsmodels.formula.api as smf
+from patsy import dmatrices
 import altair as alt
 
 from .basic import BasicPlot
@@ -7,14 +9,15 @@ from ..model.builder.cr_builder import CentralRegBuilder
 
 def basic_reg(
     data            : pd.DataFrame,
-    x               : str,
-    y               : str,
+    x:str,
+    y:str,
+    formula         : str   = 'y~x',
     reg_method      : str   = 'OLS',
-    reg_param       : dict  = None,
     ci_level        : float = 0.95,
-    pi_level        : float = 0.95,
+    pi_level        : float = None,
     int_method      : str   = 'parametric',
-    show_stats_info : bool  = True
+    show_stats_info : bool  = True,
+    qr_quantile     : float = 0.5
 ) -> alt.Chart:
     ...
     # reg_method OLS / QR
@@ -26,4 +29,32 @@ def basic_reg(
     #   OLS_spline
 
     # int_method = parametric / bootstrap / None
+    
+    formula = formula.replace('y',y).replace('x',x)
+    reg_method = reg_method.upper()
+    if reg_method == 'OLS':
+        mod = smf.ols(formula=formula,data=data)
+        mod_result = mod.fit()
+    elif reg_method == 'QR':
+        mod = smf.quantreg(formula=formula,data=data)
+        mod_result = mod.fit(q=qr_quantile)
+    else:
+        raise Exception('reg_method must in ["OLS","QR"]')
+    
+    mod_data = mod_result.get_prediction(data).summary_frame()
+    mod_data[x] = data.loc[:,x]
+    
+    basic_plot = BasicPlot(data=mod_data,x=x,y='mean')
+    plot = basic_plot.line(y_title=y,x_title=x)
+    
+    if ci_level is not None or pi_level is not None:
+        if pi_level is not None:
+            y_up,y_down = 'obs_ci_upper','obs_ci_lower'
+        else:
+            y_up,y_down = 'mean_ci_upper','mean_ci_lower'
+        band = basic_plot.error_band(y_up=y_up,y_down=y_down)
+        plot = band + plot
+    
+    return plot
+    
     
