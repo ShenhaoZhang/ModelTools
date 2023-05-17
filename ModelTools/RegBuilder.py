3e7664496f6d4789ab70cbefb7ce43f34acad176
ZhangShenhao <zhangshenhao@outlook.com>
2023-05-18 00:34:50 +0800

    update

--- ModelTools/RegBuilder.py
+++ ModelTools/RegBuilder.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 import numpy as np
+import pandas as pd
 from sklearn import linear_model as lm
 from sklearn import tree
 from sklearn import ensemble as en
@@ -14,6 +15,7 @@ from sklearn.model_selection import (
     KFold
 )
 
+
 class RegBuilder:
     
     model = {
@@ -26,13 +28,14 @@ class RegBuilder:
         'RF'   : en.RandomForestRegressor(random_state=0),
     }
     preprocess = {
-        'std'  : pr.StandardScaler(),
-        'poly' : pr.PolynomialFeatures(),
-        'inter': pr.PolynomialFeatures(interaction_only=True),
-        'sp'   : pr.SplineTransformer(),
-        'pca'  : de.PCA()
+        'std'    : pr.StandardScaler(),
+        'poly'   : pr.PolynomialFeatures(),
+        'inter'  : pr.PolynomialFeatures(interaction_only=True),
+        'sp'     : pr.SplineTransformer(),
+        'pca'    : de.PCA(),
+        'fml'    : pr.FunctionTransformer()
     }
-    param = {
+    cv_param = {
         'poly__degree'         : [2,3,4],
         'inter__degree'        : [2,3],
         'sp__extrapolation'    : ['constant','continue','linear'],
@@ -49,73 +52,90 @@ class RegBuilder:
     
     def __init__(
         self,
-        method:Union[str,dict,Pipeline] = 'poly_OLS'
+        method : Union[str,dict,Pipeline] = 'poly_OLS',
+        method_args : dict = None,
+        formula:str = None
     ) -> None:
         
-        # 两种method：
-        # list ：poly_OLS
-        # dict ：{'poly':{'degree':2},'OLS':{}}
-        
-        self.method    = method
-        self.estimator = None
-        
-        self.init_pipe()
+        self.method      = method
+        self.method_args = method_args
+        self.formula     = formula
+        self.init_pipeline()
     
-    def init_pipe(self) -> None:
-        if isinstance(self.method,str):
-            method = {}
-            method_split = self.method.split('_')
-            for mth in method_split:
-                param       = {name:param for name,param in self.param.items() if name.split('__')[0]==mth}
-                method[mth] = param
-            self.method = method
-        elif isinstance(self.method,dict):
-            method = {}
-            for mth,param in self.method.items():
-                adj_param = {f'{mth}__{n}':v for n,v in param.items()}
-                method[mth] = adj_param
-            self.method = method
-        elif isinstance(self.method,Pipeline):
-            self.pipe   = self.method
-            self.method = {}
-            return
+        # method type
+        # str1  ：poly_OLS
+        # str2  : fml_OLS
+        # pipe
+    
+    def init_pipeline(self):
+        if isinstance(self.method,Pipeline):
+            self.pipeline = Pipeline
+            return None
         
-        pipe = []
-        for mth in self.method.keys():
-            step = self.preprocess.get(mth,self.model.get(mth))
-            if step is None:
-                raise Exception(f'未发现{mth}')
-            pipe.append((mth,step))
-        self.pipe = Pipeline(steps=pipe)
+        steps = []
+        for pipe_name in self.method.split('_'):
+            estimator = self.preprocess.get(pipe_name,self.model.get(pipe_name))
+            if estimator is None:
+                raise Exception(f'未发现{pipe_name}')
+            steps.append((pipe_name,estimator))
+        self.pipeline = Pipeline(steps)
+    
+    def update_pipeline(self,estimator_param:dict):
+        # 更新pipeline中的参数
+        # estimator_param : {'poly__degree':[1]}
+        self.pipeline.set_params(**estimator_param)
+    
+    def get_cv_param_grid(self):
+        cv_param_grid = {}
+        for pipe_name in self.method.split('_'):
+            for param_name,param_value in self.cv_param.items():
+                if pipe_name in param_name.split('__')[0]:
+                    cv_param_grid.update({param_name:param_value})
+        return cv_param_grid
     
     def fit(
         self,
-        X,y,
+        X : np.ndarray | pd.DataFrame,
+        y : np.ndarray | pd.DataFrame,
         cv_method   = 'kfold',
         cv_n_splits = 5,
         cv_shuffle  = False,
         cv_score    = 'mse'
     ):
+        if isinstance(X,np.ndarray):
+            col_name = [f'x{i}' for i in range(X.shape[1])]
+            self.X = pd.DataFrame(data=X,columns=col_name)
+        elif isinstance(X,pd.DataFrame):
+            self.X = X
+        self.y = y
         
-        param_grid = {}
-        for param in self.method.values():
-            param_grid.update(param)
+        if self.formula is not None:
+            mod = self.method.split('_')[-1]
+            self.update_pipeline({
+                'fml__func'             : formula_transform,
+                'fml__kw_args'          : {'formula':self.formula},
+                'fml__feature_names_out': lambda x,y: fml_feature_names_out(x,y,formula=self.formula),
+            })
+            try:
+                self.update_pipeline({f'{mod}__fit_intercept':False})
+            except:
+                pass
         
-        cv_method = get_cv_method(cv_method,cv_n_splits,cv_shuffle)
-        cv_score  = get_cv_score(cv_score)
+        cv_param    = self.get_cv_param_grid()
+        cv_n_splits = 2 if len(cv_param) == 0 else cv_n_splits
+        cv_method   = get_cv_method(cv_method,cv_n_splits,cv_shuffle)
+        cv_score    = get_cv_score(cv_score)
         self.cv = GridSearchCV(
-            estimator  = self.pipe,
-            param_grid = param_grid,
+            estimator  = self.pipeline,
+            param_grid = cv_param,
             scoring    = cv_score,
             n_jobs     = -1,
             refit      = True,
             cv         = cv_method,
         )
         
-        self.cv.fit(X,y)
-        self.X    = X
-        self.y    = y
-        self.coef = get_coef(self.cv)
+        self.cv.fit(self.X,self.y)
+        self.coef = get_coef(self.cv,fit_intercept=False) if self.formula is not None else get_coef(self.cv)
         
         return self
         
@@ -125,7 +145,9 @@ class RegBuilder:
         pred = self.cv.predict(X)
         return pred
     
-    def predict_interval(self,X,alpha=0.05) -> tuple:
+    def predict_interval(self,X=None,alpha=0.05) -> tuple:
+        if X is None:
+            X = self.X 
         #TODO 保存模型，避免重复训练
         #TODO 选择最好的区间预测方法
         from mapie.regression import MapieRegressor
@@ -134,18 +156,23 @@ class RegBuilder:
         pred_up       = pred_interval[:,1,:].flatten()
         return pred_low,pred_up
 
+    def init_data(self,X):
+        if isinstance(X,np.ndarray):
+            col_name = [f'x{i}' for i in range(X.shape[1])]
+            self.X = pd.DataFrame(data=X,columns=col_name)
+        elif isinstance(X,pd.DataFrame):
+            self.X = X
+        self.y = y
+        return self.X,self.Y
 
 
-def get_coef(cv) -> dict:
-    try:
-        coef_value = getattr(cv.best_estimator_[-1],'coef_',[])
-        coef_name  = cv.best_estimator_[:-1].get_feature_names_out()
-        coef_name  = [f'x{i}' for i in range(len(coef_value))] if coef_name is None else coef_name
-        coef       = dict(zip(coef_name,coef_value))
-        intercept  = {'intercept_':getattr(cv.best_estimator_[-1],'intercept_',[])}
-        coef       = {**intercept,**coef}
-    except:
-        coef = None
+def get_coef(cv,fit_intercept=True) -> dict:
+    coef_value = getattr(cv.best_estimator_[-1],'coef_',[])
+    coef_name  = cv.best_estimator_[:-1].get_feature_names_out()
+    coef_name  = [f'x{i}' for i in range(len(coef_value))] if coef_name is None else coef_name
+    coef       = dict(zip(coef_name,coef_value))
+    intercept  = {'intercept_':getattr(cv.best_estimator_[-1],'intercept_',[])}
+    coef       = {**intercept,**coef} if fit_intercept == True else {**coef}
     return coef
 
 def get_cv_method(method_name,n_splits,cv_shuffle=False):
@@ -170,6 +197,15 @@ def get_cv_score(score_name:str):
     }
     score = score.get(score_name)
     return score
+
+def formula_transform(data:pd.DataFrame,formula:str):
+    from formulaic import model_matrix
+    return model_matrix(formula,data)   
+
+def fml_feature_names_out(transformer,input_name,formula):
+    data = pd.DataFrame(columns=input_name,data=np.ones(len(input_name)).reshape(1,-1))
+    output = formula_transform(data,formula)
+    return output.columns.to_list()
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
